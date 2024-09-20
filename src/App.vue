4f37b1d80e95c5728d965f36c00dee5a6e9f4264
jacopolegovini <legovini96@gmail.com>
2024-09-20 15:08:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Main</h2>
+</template>
+
+<style scoped></style>
