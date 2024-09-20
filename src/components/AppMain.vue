11f7c8413c8096040a22d4db4645e2ebe5c2bef2
jacopolegovini <legovini96@gmail.com>
2024-09-20 20:55:49 +0200

    add condition to searching method

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,10 +16,13 @@ export default {
 
 <template>
     <main>
+        <div v-if="store.errorMessage">Errore</div>
         <ul class="film-results" v-for="(foundFilm, index) in store.foundFilms.results" :key="index">
             <li>{{ foundFilm.title }}</li>
             <li>{{ foundFilm.original_title }}</li>
             <li>{{ foundFilm.original_language }}: <lang-flag :iso=foundFilm.original_language /></li>
+            <!-- TODO Da trovare un modo per trattare il caso in cui non sia presente la nazione -->
+            <!-- <li v-if="store.nationalities !== ''">test</li> -->
             <li>{{ foundFilm.vote_average }}</li>
         </ul>
     </main>
--- src/components/store.js
+++ src/components/store.js
@@ -6,6 +6,7 @@ export const store = reactive({
     foundFilms: {},
     searchedFilm: '',
     apiCallComplete: '',
+    errorMessage: false,
 
     // Methods
     // Metodo che mi permette di richiamare la API, non più utile
@@ -35,7 +36,63 @@ export const store = reactive({
             .then((response) => {
                 console.log(apiCallComplete)
                 console.log(response.data)
+
+                // Condizione qualora la ricerca non portasse a nulla
+                if (response.data.results.length === 0) {
+                    console.log('errore')
+                    this.foundFilms = ''
+                    return this.errorMessage = true
+                }
+
+                // Resetta la variabile qualora si ricercasse dopo una ricerca con esito negativo
+                this.errorMessage = false
+
+                // Setta la variabile con l'oggetto desiderato
                 this.foundFilms = response.data
             })
+    },
+    nationalities: {
+        am: 'et',
+        ar: 'sy',
+        az: 'az',
+        bn: 'bd',
+        be: 'by',
+        ca: 'es-ct',
+        cs: 'cz',
+        de: 'de',
+        el: 'gr',
+        en: 'gb',
+        es: 'es',
+        et: 'ee',
+        fa: 'ir',
+        fr: 'fr',
+        bg: 'bg',
+        ha: 'ne',
+        hi: 'in',
+        hu: 'hu',
+        hy: 'am',
+        it: 'it',
+        id: 'id',
+        ja: 'jp',
+        jv: 'id',
+        km: 'kh',
+        ko: 'kr',
+        lv: 'lv',
+        mr: 'in',
+        ms: 'my',
+        nl: 'nl',
+        pl: 'pl',
+        pt: 'pt',
+        ro: 'ro',
+        ru: 'ru',
+        sw: 'ke',
+        ta: 'lk',
+        te: 'in',
+        th: 'th',
+        tr: 'tr',
+        uk: 'ua',
+        uz: 'uz',
+        vi: 'vn',
+        zh: 'cn',
     }
 })
