8ca128624bfcdf583bd0e25c00f72c780daab781
jacopolegovini <legovini96@gmail.com>
2024-09-21 11:11:15 +0200

    issue with not included flag fixed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,20 +10,33 @@ export default {
     },
     components: {
         LangFlag
+    },
+    methods: {
+        // Metodo per risolvere il problema delle lingue senza bandiera
+        createKeyObject(foundFilm = false, foundSerie = false) {
+            const keys = Object.keys(store.nationalities)
+            const keysMovie = foundFilm.original_language
+            const keysSerie = foundSerie.original_language
+
+            if (foundFilm) return keys.includes(keysMovie)
+            if (foundSerie) return keys.includes(keysSerie)
+        }
     }
 }
 </script>
 
 <template>
     <main>
+        
         <div v-if="store.errorMessage">Errore</div>
         <div v-if="store.typeOfEntertainment === 'film'">
             <ul class="film-results" v-for="(foundFilm, index) in store.foundFilms.results" :key="index">
                 <li>{{ foundFilm.title }}</li>
                 <li>{{ foundFilm.original_title }}</li>
-                <li>{{ foundFilm.original_language }}: <lang-flag :iso=foundFilm.original_language /></li>
-                <!-- TODO Da trovare un modo per trattare il caso in cui non sia presente la nazione -->
-                <!-- <li v-if="store.nationalities !== ''">test</li> -->
+                <li v-if="createKeyObject(foundFilm)">
+                    <lang-flag :iso=foundFilm.original_language />
+                </li>
+                <li v-else>{{ foundFilm.original_language }}</li>
                 <li>{{ foundFilm.vote_average }}</li>
             </ul>
         </div>
@@ -31,9 +44,9 @@ export default {
             <ul v-for="(foundSerie, index) in store.foundSeries.results" :key="index">
                 <li>{{ foundSerie.name }}</li>
                 <li>{{ foundSerie.original_name }}</li>
-                <li>{{ foundSerie.original_language }}: <lang-flag :iso=foundSerie.original_language /></li>
+                <li v-if="createKeyObject(foundSerie)"><lang-flag :iso=foundSerie.original_language /></li>
+                <li v-else>{{ foundSerie.original_language }}</li>
                 <li>{{ foundSerie.vote_average }}</li>
-    
             </ul>
         </div>
     </main>
